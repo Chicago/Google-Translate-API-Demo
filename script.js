6b78b3b8085ea9dfb373ef9db2d67d7ecd0556fd
abby-lammers <abigaillammers2019@u.northwestern.edu>
2019-08-14 15:29:13 -0500

    Translate one block of text into the 5 ordinance languages

---
+++ script.js
@@ -0,0 +1,28 @@
+API_KEY = "AIzaSyCK8pHpO7M5mTezRxMlos4AftAbvTkzuFY"
+
+var allElem = document.getElementsByTagName("*");
+for (var i = 0; i < allElem.length; i++) {
+    // Do something with all element here
+}
+
+
+$("#translateButton").click(function () {
+
+  var url = "https://translation.googleapis.com/language/translate/v2";
+  url += "/?key=" + API_KEY;
+  url += "&target=" + $("#targetLanguage").val(); // https://stackoverflow.com/questions/50719010/google-translate-api-translate-page-using-js
+  url += "&q=" + encodeURI($("#translateText").text());
+
+  var request = new XMLHttpRequest();
+  request.open('POST', url);
+  request.onload = function () {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+  if (request.status >= 200 && request.status < 400) {
+    $("#translateText").text(data.data.translations[0].translatedText);
+  }
+}
+
+request.send();
+
+});
