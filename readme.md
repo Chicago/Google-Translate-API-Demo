b6c4a6fa18a2854b4c964ef61b069931d52a7736
abby-lammers <abigaillammers2019@u.northwestern.edu>
2019-08-16 09:00:34 -0500

    Improved readme and comments

--- readme.md
+++ readme.md
@@ -1,4 +1,12 @@
 # Google Translate API Demo
-A proof-of-concept for using Google translate in the browser (without caching translations) for web content. 
+A proof-of-concept for using Google translate in the browser (without caching translations) for web content.
 
 ![GIF of translate demo](/img/demo-recording.gif)
+
+## Motivation
+
+The City of Chicago is developing a web content translation strategy to comply with the [2015 language access ordinance](https://www.chicago.gov/content/dam/city/depts/mayor/Office%20of%20New%20Americans/PDFs/Language%20Access%20Ordinance.pdf). This repo demonstrates the viability of the [Google Translate v2 API](https://cloud.google.com/translate/docs/reference/rest/v2/translate) for translating web content. 
+
+## Next Steps
+
+This demo is a minimal proof-of-concept for web-based translation. A more complete script would scan the DOM for text objects, make one API call containing all text to be translated, and replace all text objects. 
--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-API_KEY = "your-api-key"
+API_KEY = "your-api-key" // replace with google api key, available from google console project
 
+// when the translate button is clicked, pull text from objects with ID "translateText" 
+// and replace with a translated version.
 $("#translateButton").click(function () {
 
+  // construct url to translate content in #translateText to the language selected by #targetLanguage 
   var url = "https://translation.googleapis.com/language/translate/v2";
   url += "/?key=" + API_KEY;
   url += "&target=" + $("#targetLanguage").val(); // https://stackoverflow.com/questions/50719010/google-translate-api-translate-page-using-js
-  url += "&q=" + encodeURI($("#translateText").text());
+  url += "&q=" + encodeURI($("#translateText").text()); // encodeURI converts strings to url-safe text
 
+  // POST to google translate api
   var request = new XMLHttpRequest();
   request.open('POST', url);
+  // after the request is complete, extract translated text and replace in the web page
   request.onload = function () {
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response);
-  if (request.status >= 200 && request.status < 400) {
-    $("#translateText").text(data.data.translations[0].translatedText);
+    var data = JSON.parse(this.response);
+    if (request.status >= 200 && request.status < 400) {
+      $("#translateText").text(data.data.translations[0].translatedText);
+    }
   }
-}
-
-request.send();
+  request.send();
 
 });
